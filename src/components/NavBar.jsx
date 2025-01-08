2a3489cb9d5aef61655aed8b910caa20895c388d
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2025-01-07 22:04:45 -0300

    comecei a home

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
+
+const NavBar = () => {
+    return (
+        <nav id="navBar">
+            <h2>
+                <Link to="/"><BiCameraMovie />Movies Library</Link>
+            </h2>
+            <Link to="/movie/1">Movie</Link>
+            <Link to="/search">Search</Link>
+            <form>
+                <input type="text" id="" placeholder="Busque um filme" />
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+
+  };
+
+  // ao carregar a página o useEffect será executado por não possuir dependências para modificar no []
+  useEffect(() => {
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedURL);
+  }, [])
+
+  return (
+    <div>
+      {topMovies && topMovies.map((movie) => <p>{ movie.title}</p>)}
+    </div>
+  )
+}
+
+export default Home
